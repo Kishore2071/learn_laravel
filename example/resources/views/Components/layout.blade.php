c6d048318c2bdcc311a84e71dadd7cc09556d9d7
Kishore2071 <kishorekumarsaran2001@gmail.com>
2024-04-07 14:05:42 +0530

    Testing Completed

--- example/resources/views/Components/layout.blade.php
+++ example/resources/views/Components/layout.blade.php
@@ -7,9 +7,9 @@
 </head>
 <body>
     <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
+        <a href="{{ route('/home') }}">Home</a>
+        <a href="{{ route('/about') }}">About</a>
+        <a href="{{ route('/contact') }}">Contact</a>
     </nav>
     
     <?php echo $slot ?>
